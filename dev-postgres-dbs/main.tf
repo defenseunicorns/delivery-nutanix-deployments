28b5c912e617e14c08c5fd6327b742e28548af54
MxNxPx <MxNxPx@gmail.com>
2024-10-18 10:14:49 -0400

    chore: add licensing and codeowners

--- dev-postgres-dbs/main.tf
+++ dev-postgres-dbs/main.tf
@@ -1,3 +1,6 @@
+# Copyright 2024 Defense Unicorns
+# SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+
 locals {
   default_software_profile_name = "fips_postgres_14"
   default_compute_profile_name  = "large-compute"
--- eks-dev-cluster/variables.tf
+++ eks-dev-cluster/variables.tf
@@ -1,3 +1,6 @@
+# Copyright 2024 Defense Unicorns
+# SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+
 variable "kubeconfig" {
   description = "Path to the kubeconfig to use for using the kubernetes provider. Should be a config for an eks-a management cluster."
   type        = string
